f08b4264eb14d1bf1ea589ecddd187574bcaaa69
hwapyeonglee <60067186+hwapyeonglee@users.noreply.github.com>
2025-08-25 17:15:13 +0900

    Initialize the project (#2)
    * Initialize the project
    
    * Configure firebase
    
    * Lint

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { API_BASE_URL } from "./config";
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState<string>("(loading...)");
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/db-ping`)
+      .then((res) => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.text();
+      })
+      .then((data) => setMessage(data))
+      .catch((err) => setMessage("Error: " + err.message));
+  }, []);
+
+  return (
+    <div className="app">
+      <header className="app-header"></header>
+      <main className="app-main">
+        <h1>knockbook-frontend</h1>
+        <p>response: {message}</p>
+      </main>
+      <footer className="app-footer"></footer>
+    </div>
+  )
+}
+
+export default App
