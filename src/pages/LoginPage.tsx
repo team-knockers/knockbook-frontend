7aca9d8d08145941a728c0989a5dd75bcb4bb0b4
hwapyeonglee <hwapyeong.lee@gmail.com>
2025-09-12 00:28:16 +0900

    Lint

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InputGroup, InputGroupText, Input, Label } from "reactstrap";
 import { FiEye, FiEyeOff } from "react-icons/fi";
