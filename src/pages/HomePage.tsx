ef41bb76c4c7691a81b257c6b40c8cc04f2bf337
hwapyeonglee <60067186+hwapyeonglee@users.noreply.github.com>
2025-09-17 15:59:52 +0900

    Add ThreeLevelTabMenu component with tests (#25)
    - implemented reusable ThreeLevelTabMenu component
    - added basic test cases for rendering and navigation behavior

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,28 @@
 import { Outlet } from "react-router-dom";
-import TwoLevelTabMenu from "../components/navigation/TwoLevelTabMenu";
 import { PATHS } from "../routes/paths";
 
+import TwoLevelTabMenu from "../components/navigation/TwoLevelTabMenu";
+import ThreeLevelTabMenu from "../components/navigation/ThreeLevelTabMenu";
+
 import s from './HomePage.module.css';
+
 
 export default function HomePage() {
   return (
     /* !caution! this is a temporary code for guide */
     <main className={s['home-layout']}>
       <TwoLevelTabMenu
-          leftTabTitle="상세보기"
-          rightTabTitle="리뷰보기"
-          leftTabPath={PATHS.homeSub1Page}
-          rightTabPath={PATHS.homeSub2Page}
-      />
+        leftTabTitle="상세정보"
+        rightTabTitle="리뷰"
+        leftTabPath={PATHS.homeSub1Page}
+        rightTabPath={PATHS.homeSub2Page}/>
+      <ThreeLevelTabMenu
+        leftTabTitle="상품설명"
+        centerTabTitle="리뷰"
+        rightTabTitle="Q&A"
+        leftTabPath={PATHS.homeSub3Page}
+        centerTabPath={PATHS.homeSub4Page}
+        rightTabPath={PATHS.homeSub5Page}/>
       <Outlet />
     </main>
   );
---
+++ src/pages/HomeSub4Page.tsx
@@ -0,0 +1,9 @@
+export default function homeSub4Page() {
+  return (
+    <main>
+      <div>
+        <h1>Knockbook HomePage - sub4</h1>
+      </div>
+    </main>
+  );
+}
