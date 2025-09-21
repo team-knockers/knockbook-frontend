15db46ebc35cdaf60872185e68850eb8ebcd3ff4
suhyeon <rainbowjane@naver.com>
2025-09-21 19:41:26 +0900

    Create ProductSummaryList component(dummy data)

--- src/features/products/components/ProductSummaryList.tsx
+++ src/features/products/components/ProductSummaryList.tsx
@@ -1,13 +1,29 @@
 import ProductSummaryCard from './ProductSummaryCard';
 import styles from './styles/ProductSummaryList.module.css';
+import { productSummaryDummy } from './DummySummary';
 
 export default function ProductSummaryList() {
+  // 임시 더미 데이터 사용 (추후 API 연동 예정)
+  const items = productSummaryDummy;
+  const handleCardClick = (id: string) => {
+    console.log('go detail:', id); // TODO: 상세 페이지로 이동
+  }
+
   return (
     <section className={styles['product-list']}>
         <div className={styles['product-list-header']}></div>
         <div className={styles['product-list-body']}>
-          {Array.from({ length: 30 }).map((_, i) => (
-              <ProductSummaryCard key={i} />
+          {items.map(p => (
+            <ProductSummaryCard
+              key={p.id}
+              imageSrc={p.imageSrc}
+              name={p.name}
+              price={p.price}
+              salePrice={p.salePrice}
+              rating={p.rating}
+              reviewCount={p.reviewCount}
+              onClick={() => handleCardClick(p.id)}
+            />
           ))}
         </div>
     </section>
