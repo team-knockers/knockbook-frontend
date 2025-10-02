c06aaa1cd738eda63c9460eabd4802197cb108fb
Yoonhwan-An <po96106@naver.com>
2025-10-02 14:50:59 +0900

    Chore: create dummy resources

---
+++ src/features/books/resources/bookDetailsPage.dummy.ts
@@ -0,0 +1,40 @@
+export const myMbtiDummy = 'ISFP';
+
+export const mbtiResearchDummy = [
+  { mbti: 'ISFP', rate: 43.4 },
+  { mbti: 'INTP', rate: 20.7 },
+  { mbti: 'ENFP', rate: 15.6 },
+  { mbti: 'INFJ', rate: 10.5 },
+  { mbti: 'ESFJ', rate: 5.7 },
+  { mbti: 'ISFJ', rate: 4.1 }
+];
+
+export const bookDetailsDummy = {
+  id: '1',
+  title: '혼모노',
+  author: '성해나',
+  publisher: '창비',
+  publishedAt: '2025-03-28',
+  categoryId: '1',
+  subcategoryId: '1',
+  introductionTitle: '도서 소개 타이틀',
+  introductionDetail: '도서 소개 디테일',
+  tableOfContents: '목차 내용',
+  publisherReview: '출판사 리뷰',
+  isbn13: '9788936439743',
+  pageCountText: '368p',
+  dimensionsText: '128 * 188(mm)',
+  weightText: '',
+  totalVolumesText: '1권',
+  rentalAmount: 2500,
+  purchaseAmount: 18000,
+  discountedPurchaseAmount: 16200,
+  coverImageUrl: 'https://contents.kyobobook.co.kr/sih/fit-in/200x0/pdt/9788936439743.jpg',
+  rentalAvailability: 'AVAILABLE',
+  purchaseAvailability: 'AVAILABLE',
+  viewCount: 0,
+  salesCount: 0,
+  rentalCount: 0,
+  averageRating: 4.5,
+  ratingCount: 217
+};
