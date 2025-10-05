d7e34e76267cafb2c6a23823c6854bf63e96d1f8
Yoonhwan-An <po96106@naver.com>
2025-10-05 17:52:35 +0900

    Chore: Move dummy data for Common use

--- src/features/books/resources/bookDetailsPage.dummy.ts
+++ src/features/books/resources/bookDetailsPage.dummy.ts
@@ -1,3 +1,26 @@
+export const bookBannersDummy = [
+  {
+    id: '1',
+    mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/b6184cd613704109a7e830ea4f75b515.jpg',
+    desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/91122066f6654d40ae83686dcb2267e5.jpg'
+  },
+  {
+    id: '2',
+    mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/71a6e24dcb274056852a83b665c77d8d.jpg',
+    desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/c9797b7b332d4670bc364e17f17a597f.jpg'
+  },
+  {
+    id: '3',
+    mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/4a317505f8364ad2b08322d77ed51823.jpg',
+    desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/32d8920d082e410bb65f661b5651b97b.jpg'
+  },
+  {
+    id: '4',
+    mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/f246603304ea4bfba8627ecc13895ee8.jpg',
+    desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/b6c6814fda0848f6b6b00cedcb684c0e.jpg'
+  }
+]; 
+
 export const myMbtiDummy = 'ISFP';
 
 export const mbtiResearchDummy = [
--- src/pages/books/BooksHomePage.tsx
+++ src/pages/books/BooksHomePage.tsx
@@ -11,12 +11,15 @@ import { useState } from "react";
 import { generatePath, useLoaderData, useNavigate } from "react-router-dom";
 import { booksHomeNewReleaseCategories, type BooksHomeLoaderData } from "./BooksHome.loader";
 import { PATHS } from "../../routes/paths";
+import { bookBannersDummy } from '../../features/books/resources/bookDetailsPage.dummy';
 
 export default function BooksHomePage() {
   const navigate = useNavigate();
   
   const { top3BestSellers, booksByCategory } = useLoaderData() as BooksHomeLoaderData;
   const [isCategoryPopupOpen, setIsCategoryPopupOpen] = useState(false);
+
+  const banners = bookBannersDummy;
   
   const handleSearch = (searchBy: 'title' | 'author' | 'publisher', searchKeyword: string) => {
     navigate(`/books/search?by=${searchBy}&keyword=${encodeURIComponent(searchKeyword)}`);
@@ -36,30 +39,6 @@ export default function BooksHomePage() {
   const handleBookItemClick = (id: string) => {
     navigate(generatePath(PATHS.bookDetails, { bookId: id }));
   };
-
-  // Dummy data for Banners
-  const banners = [
-    {
-      id: '1',
-      mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/b6184cd613704109a7e830ea4f75b515.jpg',
-      desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/91122066f6654d40ae83686dcb2267e5.jpg'
-    },
-    {
-      id: '2',
-      mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/71a6e24dcb274056852a83b665c77d8d.jpg',
-      desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/c9797b7b332d4670bc364e17f17a597f.jpg'
-    },
-    {
-      id: '3',
-      mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/4a317505f8364ad2b08322d77ed51823.jpg',
-      desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/32d8920d082e410bb65f661b5651b97b.jpg'
-    },
-    {
-      id: '4',
-      mobileImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/f246603304ea4bfba8627ecc13895ee8.jpg',
-      desktopImageUrl: 'https://contents.kyobobook.co.kr/pmtn/2025/event/b6c6814fda0848f6b6b00cedcb684c0e.jpg'
-    }
-  ]; 
 
   return (
     <>
