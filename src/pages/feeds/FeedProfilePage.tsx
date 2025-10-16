89f19f61e38d4525c45356bf79f20362fbc6bc65
suhyeon <rainbowjane@naver.com>
2025-10-16 15:17:30 +0900

    Hotfix

--- src/pages/feeds/FeedProfilePage.tsx
+++ src/pages/feeds/FeedProfilePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import s from './FeedProfilePage.module.css';
